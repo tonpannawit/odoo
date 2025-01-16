ee78337f89fcbbcbb7c83feb96fd7ad9f4ccbf70
tonpannawit <114065969+tonpannawit@users.noreply.github.com>
2025-01-17 08:41:26 +1100

    Add files via upload

---
+++ search_by_phone_number_by_ton/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Search Partners by Phone/Mobile Number',
+    'version': '1.1',
+    'category': 'Sales',
+    'summary': 'Search Partners by phone or mobile number',
+    'description': """
+    """,
+    'license': 'LGPL-3',
+    'author': 'Ton Pannawit',
+    'website': '',
+    'depends': ['base', 'stock', 'product', 'sale_management'],
+    'data': [],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
